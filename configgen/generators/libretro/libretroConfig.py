af38ad9461b0a31103da142f9b0ff133ac1c8309
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-11-03 20:50:40 +0100

    mame: redisable rewinding
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- configgen/generators/libretro/libretroConfig.py
+++ configgen/generators/libretro/libretroConfig.py
@@ -36,7 +36,7 @@ coreToP2Device = {'fuse': '513'};
 systemToRetroachievements = {'snes', 'nes', 'gba', 'gb', 'gbc', 'megadrive', 'pcengine'};
 
 # Define systems not compatible with rewind option
-systemNoRewind = {'sega32x', 'psx', 'zxspectrum', 'odyssey2', 'n64'};
+systemNoRewind = {'sega32x', 'psx', 'zxspectrum', 'odyssey2', 'mame', 'n64'};
 
 # Define system emulated by bluemsx core
 systemToBluemsx = {'msx': '"MSX2"', 'msx1': '"MSX2"', 'msx2': '"MSX2"', 'colecovision': '"COL - ColecoVision"' };
